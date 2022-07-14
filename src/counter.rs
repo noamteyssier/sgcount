bbe95f96d79a511ebbfe0164ff84d861a0e58ff4
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-07-14 11:28:49 -0700

    added counter testing with distances

--- src/counter.rs
+++ src/counter.rs
@@ -32,3 +32,54 @@ impl Counter {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+
+    use fxread::{FastaReader, FastxRead, Record};
+    use super::{Library, Trimmer, Counter};
+
+    fn trim_reader(distance: bool) -> Box<dyn FastxRead<Item = Record>> {
+        let sequence: &'static [u8] = match distance {
+            false => b">seq.0\nACTG\n",
+            true => b">seq.0\nAGTG\n",
+        };
+        Box::new(FastaReader::new(sequence))
+    }
+
+    fn lib_reader() -> Box<dyn FastxRead<Item = Record>> {
+        let sequence: &'static [u8] = b">seq.0\nACTG\n";
+        Box::new(FastaReader::new(sequence))
+    }
+
+    fn trimmer(distance: bool) -> Trimmer {
+        Trimmer::from_reader(trim_reader(distance), 0, 4)
+    }
+
+    fn library() -> Library {
+        Library::from_reader(lib_reader()).unwrap()
+    }
+
+    #[test]
+    fn count_no_distance() {
+        let no_distance_trimmer = trimmer(false);
+        let distance_trimmer = trimmer(true);
+        let library = library();
+        let no_distance_count = Counter::new(no_distance_trimmer, &library, 0);
+        let distance_count = Counter::new(distance_trimmer, &library, 0);
+        assert_eq!(*no_distance_count.get_value("seq.0"), 1);
+        assert_eq!(*distance_count.get_value("seq.0"), 0);
+    }
+
+    #[test]
+    fn count_with_distance() {
+        let no_distance_trimmer = trimmer(false);
+        let distance_trimmer = trimmer(true);
+        let library = library();
+        let no_distance_count = Counter::new(no_distance_trimmer, &library, 1);
+        let distance_count = Counter::new(distance_trimmer, &library, 1);
+        assert_eq!(*no_distance_count.get_value("seq.0"), 1);
+        assert_eq!(*distance_count.get_value("seq.0"), 1);
+    }
+
+}
