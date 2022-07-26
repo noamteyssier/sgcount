ef3830e89b0dbc194b14aafc1edf959b18942c77
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-07-25 19:26:06 -0700

    converted all vec and string operations to u8

--- src/permutes.rs
+++ src/permutes.rs
@@ -1,6 +1,6 @@
 use hashbrown::{HashSet, HashMap};
 
-const LEXICON: [char; 5] = ['A', 'C', 'G', 'T', 'N'];
+const LEXICON: [u8; 5] = [b'A', b'C', b'G', b'T', b'N'];
 
 /// Calculates all unambiguous single edit distance permutations 
 /// for a set of sequences (hamming distance = 1)
@@ -30,27 +30,27 @@ const LEXICON: [char; 5] = ['A', 'C', 'G', 'T', 'N'];
 ///  `CG`).
 ///
 pub struct Permuter {
-    map: HashMap<String, String>,
-    _null: HashSet<String>
+    map: HashMap<Vec<u8>, Vec<u8>>,
+    _null: HashSet<Vec<u8>>
 }
 
 impl Permuter {
 
     /// Initiates the algorithm to determine all unambiguous one-off sequences
     /// from an iterator of sequences. This input can be anything which implements
-    /// the [`Iterator`] trait on [`String`] references. 
+    /// the [`Iterator`] trait on [`Vec<u8>`] references. 
     ///
     /// The internal `map` relates child permuted sequences with their parent sequences. 
     /// The internal `_null` is a `HashSet` of all parent sequences as well as all ambiguous
     /// one-offs.
-    pub fn new<'a>(sequences: impl Iterator<Item = &'a String>) -> Self {
+    pub fn new<'a>(sequences: impl Iterator<Item = &'a Vec<u8>>) -> Self {
         let (map, null) = Self::build(sequences);
         Self { map, _null: null }
     }
 
     /// Publically exposes the internal [`HashMap`] to recover the parent sequence
     /// of a potential permuted sequence.
-    pub fn contains(&self, token: &str) -> Option<&String> {
+    pub fn contains(&self, token: &[u8]) -> Option<&Vec<u8>> {
         self.map.get(token)
     }
 
@@ -59,7 +59,7 @@ impl Permuter {
     /// These are then folded into the `map` and `_null` data types depending on the predicate
     /// described in [`Self::insert_sequence`]
     fn build<'a>(
-            sequences: impl Iterator<Item = &'a String>) -> (HashMap<String, String>, HashSet<String>) 
+            sequences: impl Iterator<Item = &'a Vec<u8>>) -> (HashMap<Vec<u8>, Vec<u8>>, HashSet<Vec<u8>>) 
     {
         sequences
             .map(|seq| (seq, Self::permute_sequence(seq, &LEXICON)))
@@ -75,11 +75,12 @@ impl Permuter {
 
     /// Generates all possible sequence permutations for a provided sequence and lexicon.
     fn permute_sequence(
-            sequence: &str, 
-            lexicon: &[char; 5]) -> Vec<String> 
+            sequence: &[u8], 
+            lexicon: &[u8; 5]) -> Vec<Vec<u8>> 
     {
         sequence
-            .char_indices()
+            .iter()
+            .enumerate()
             .map(|(idx, _)| Self::sequence_regions(sequence, idx))
             .map(|(p,x,s)| Self::build_permutations(p, s, x, lexicon))
             .flatten()
@@ -89,8 +90,8 @@ impl Permuter {
     /// Splits a sequence into thirds at a specific index and returns the prefix,
     /// suffix, and basepair at that index.
     fn sequence_regions(
-            sequence: &str, 
-            idx: usize) -> (&str, &str, &str) 
+            sequence: &[u8], 
+            idx: usize) -> (&[u8], &[u8], &[u8]) 
     {
         let (prefix, poschar) = sequence.split_at(idx);
         let (_, suffix) = sequence.split_at(idx + 1);
@@ -99,34 +100,29 @@ impl Permuter {
 
     /// Generates all permutations at a specific index within the sequence.
     fn build_permutations(
-            prefix: &str, 
-            suffix: &str, 
-            poschar: &str, 
-            lexicon: &[char; 5]) -> Vec<String> 
+            prefix: &[u8], 
+            suffix: &[u8], 
+            poschar: &[u8], 
+            lexicon: &[u8; 5]) -> Vec<Vec<u8>> 
     {
         lexicon
             .iter()
-            .filter(move |y| *y != &Self::aschar(poschar))
+            .filter(move |y| **y != poschar[0])
             .map(|y| Self::build_permutation(prefix, suffix, y))
             .collect()      
-    }
-
-    /// Utility function to convert a [`&str`] -> [`char`]
-    fn aschar(poschar: &str) -> char {
-        poschar.chars().next().unwrap()
     }
 
     /// Creates a specific permutation by stitching together a prefix,
     /// basepair, and suffix
     fn build_permutation(
-            prefix: &str, 
-            suffix: &str, 
-            insertion: &char) -> String 
+            prefix: &[u8], 
+            suffix: &[u8], 
+            insertion: &u8) -> Vec<u8> 
     {
-        let mut sequence = String::new();
-        sequence.push_str(prefix);
+        let mut sequence = Vec::new();
+        sequence.extend_from_slice(prefix);
         sequence.push(*insertion);
-        sequence.push_str(suffix);
+        sequence.extend_from_slice(suffix);
         sequence
     }
 
@@ -139,13 +135,13 @@ impl Permuter {
     /// If it is not found in the `map` set then add it to the map set and link it to its
     /// parent sequence.
     fn insert_sequence(
-            sequence: &str, 
-            permutation: &String, 
-            null: &mut HashSet<String>, 
-            table: &mut HashMap<String, String>) 
+            sequence: &[u8], 
+            permutation: &Vec<u8>, 
+            null: &mut HashSet<Vec<u8>>, 
+            table: &mut HashMap<Vec<u8>, Vec<u8>>) 
     {
         if !null.contains(sequence) {
-            null.insert(sequence.to_string());
+            null.insert(sequence.to_owned());
         }
 
         if !null.contains(permutation) {
@@ -160,9 +156,9 @@ impl Permuter {
     /// This will remove that sequence from the `map` set and insert it into the `null`
     /// set so it cannot be used again.
     fn insert_to_null(
-            permutation: &String, 
-            null: &mut HashSet<String>, 
-            table: &mut HashMap<String, String>) 
+            permutation: &Vec<u8>, 
+            null: &mut HashSet<Vec<u8>>, 
+            table: &mut HashMap<Vec<u8>, Vec<u8>>) 
     {
         table.remove(permutation);
         null.insert(permutation.to_owned());
@@ -171,11 +167,11 @@ impl Permuter {
     /// Case when a newly generated permutation has not been seen before. This then
     /// adds it to the `map` set and links it to its parent sequence.
     fn insert_to_table(
-            permutation: &String, 
-            sequence: &str, 
-            table: &mut HashMap<String, String>) 
+            permutation: &Vec<u8>, 
+            sequence: &[u8], 
+            table: &mut HashMap<Vec<u8>, Vec<u8>>) 
     {
-        table.insert(permutation.to_owned(), sequence.to_string());
+        table.insert(permutation.to_owned(), sequence.to_owned());
     }
 }
 
@@ -208,62 +204,67 @@ mod test {
 
     #[test]
     fn build() {
-        let sequences = vec!["AC".to_string(), "CG".to_string()];
+        let sequences = vec![b"AC".to_vec(), b"CG".to_vec()];
         Permuter::new(sequences.iter());
     }
 
     #[test]
     fn validate_singleton() {
-        let sequences = vec!["ACTG".to_string()];
+        let sequences = vec![b"ACTG".to_vec()];
         let permuter = Permuter::new(sequences.iter());
-        let truth = vec![
-            "AATG","ACGG","ACAG","TCTG",
-            "ACNG","NCTG","ACTA","GCTG",
-            "AGTG","ACTC","ATTG","ANTG",
-            "ACCG","ACTT","CCTG","ACTN"
-        ];
-        assert!(truth.iter().all(|x| permuter.map.contains_key(*x)));
-        assert!(truth.iter().all(|x| !permuter._null.contains(*x)));
-        assert!(permuter._null.contains("ACTG"));
+        let truth: Vec<Vec<u8>> = vec![
+            b"AATG",b"ACGG",b"ACAG",b"TCTG",
+            b"ACNG",b"NCTG",b"ACTA",b"GCTG",
+            b"AGTG",b"ACTC",b"ATTG",b"ANTG",
+            b"ACCG",b"ACTT",b"CCTG",b"ACTN"
+        ].iter().map(|s| s.to_vec()).collect();
+        assert!(truth.iter().all(|x| permuter.map.contains_key(x)));
+        assert!(truth.iter().all(|x| !permuter._null.contains(x)));
+        assert!(permuter._null.contains(&b"ACTG".to_vec()));
         assert_eq!(permuter._null.len(), 1);
     }
 
     #[test]
     fn validate_positive() {
-        let sequences = vec!["AC".to_string(), "CG".to_string()];
+        let sequences = vec![b"AC".to_vec(), b"CG".to_vec()];
         let permuter = Permuter::new(sequences.iter());
 
-        let known_positives = vec!["GC", "TC", "NC", "AA", "AT", "AN", "CA", "CT", "CN", "GG", "TG", "NG"];
+        let known_positives: Vec<Vec<u8>> = vec![
+            b"GC", b"TC", b"NC", b"AA", 
+            b"AT", b"AN", b"CA", b"CT", 
+            b"CN", b"GG", b"TG", b"NG"].iter().map(|s| s.to_vec()).collect();
 
         // validate known positives in map
         known_positives
             .iter()
-            .for_each(|x| assert!(permuter.map.contains_key(*x)));
+            .for_each(|x| assert!(permuter.map.contains_key(x)));
         assert_eq!(permuter.map.len(), 12);
 
         // validate known negatives not in map
         known_positives 
             .iter()
-            .for_each(|x| assert!(!permuter._null.contains(*x)));
+            .for_each(|x| assert!(!permuter._null.contains(x)));
     }
 
     #[test]
     fn validate_negative() {
-        let sequences = vec!["AC".to_string(), "CG".to_string()];
+        let sequences = vec![b"AC".to_vec(), b"CG".to_vec()];
         let permuter = Permuter::new(sequences.iter());
 
-        let known_negatives = vec!["AG", "CG", "CC", "AG"];
+        let known_negatives: Vec<Vec<u8>> = vec![
+            b"AG", b"CG", b"CC", b"AG"
+        ].iter().map(|s| s.to_vec()).collect();
 
         // validate known negatives in._null
         known_negatives
             .iter()
-            .for_each(|x| assert!(permuter._null.contains(*x)));
+            .for_each(|x| assert!(permuter._null.contains(x)));
         assert_eq!(permuter._null.len(), 4);
 
         // validate known positives not in._null
         known_negatives
             .iter()
-            .for_each(|x| assert!(!permuter.map.contains_key(*x)));
+            .for_each(|x| assert!(!permuter.map.contains_key(x)));
     }
 
 }
