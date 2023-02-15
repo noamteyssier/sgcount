215cec12fb1511e2b7e4c1dec45b6fb39c8d1e33
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-02-15 09:43:23 -0800

    split loading into from file and from buffer for easier testing + adde testing through each component

--- src/genemap.rs
+++ src/genemap.rs
@@ -6,6 +6,7 @@ use std::{fs::File, io::BufReader, path::Path};
 
 /// Container to handle mapping of gene identifiers and
 /// `sgRNA` identifiers
+#[derive(Debug)]
 pub struct GeneMap {
     map: HashMap<Vec<u8>, Vec<u8>>,
 }
@@ -19,7 +20,13 @@ impl GeneMap {
     /// Creates a new genemap from a filepath
     pub fn new(path: &str) -> Result<Self> {
         Self::validate_path(path)?;
-        let map = Self::build(path)?;
+        let map = Self::build_from_file(path)?;
+        Ok(Self { map })
+    }
+
+    /// Creates a new genemap from a buffer
+    pub fn new_from_buffer<R: BufReadExt>(buffer: R) -> Result<Self> {
+        let map = Self::build(buffer)?;
         Ok(Self { map })
     }
 
@@ -35,10 +42,15 @@ impl GeneMap {
         }
     }
 
-    /// Processes the tab delim file and panics if tabs are not found or duplicate `sgRNAs` are found
-    fn build(path: &str) -> Result<HashMap<Vec<u8>, Vec<u8>>> {
+    /// Builds the gene map from a file
+    fn build_from_file(path: &str) -> Result<HashMap<Vec<u8>, Vec<u8>>> {
         let file = File::open(path)?;
-        let mut buffer = BufReader::new(file);
+        let buffer = BufReader::new(file);
+        Self::build(buffer)
+    }
+
+    /// Processes the tab delim file and panics if tabs are not found or duplicate `sgRNAs` are found
+    fn build<R: BufReadExt>(mut buffer: R) -> Result<HashMap<Vec<u8>, Vec<u8>>> {
         let mut map = HashMap::new();
         buffer.for_byte_line(|line| {
             let pos = line
@@ -67,4 +79,74 @@ impl GeneMap {
     pub fn validate_library(&self, library: &Library) -> bool {
         library.values().all(|alias| self.get(alias).is_some())
     }
+}
+
+#[cfg(test)]
+mod testing {
+    use hashbrown::HashMap;
+
+    use crate::Library;
+
+
+    fn build_example_buffer() -> String {
+        "gene1\tsgrna1\n\
+         gene2\tsgrna2\n\
+         gene3\tsgrna3\n"
+            .to_string()
+    }
+
+    fn build_library() -> Library {
+        let map = vec![
+            (b"ACTG".to_vec(), b"sgrna1".to_vec()),
+            (b"gtca".to_vec(), b"sgrna2".to_vec()),
+            (b"TCAG".to_vec(), b"sgrna3".to_vec()),
+        ]
+        .into_iter()
+        .collect::<HashMap<_, _>>();
+        Library::from_hashmap(map).unwrap()
+    }
+
+    fn build_invalid_library() -> Library {
+        let map = vec![
+            (b"ACTG".to_vec(), b"sgrna1".to_vec()),
+            (b"gtca".to_vec(), b"sgrna4".to_vec()),
+        ]
+        .into_iter()
+        .collect::<HashMap<_, _>>();
+        Library::from_hashmap(map).unwrap()
+    }
+
+    #[test]
+    fn test_build() {
+        let buffer = build_example_buffer();
+        let genemap = super::GeneMap::new_from_buffer(buffer.as_bytes()).unwrap();
+        assert_eq!(genemap.get(b"sgrna1").unwrap(), &b"gene1"[..]);
+        assert_eq!(genemap.get(b"sgrna2").unwrap(), &b"gene2"[..]);
+        assert_eq!(genemap.get(b"sgrna3").unwrap(), &b"gene3"[..]);
+    }
+
+    #[test]
+    fn test_validate_library() {
+        let buffer = build_example_buffer();
+        let genemap = super::GeneMap::new_from_buffer(buffer.as_bytes()).unwrap();
+        let library = build_library();
+        assert!(genemap.validate_library(&library));
+    }
+
+    #[test]
+    fn test_validate_library_invalid() {
+        let buffer = build_example_buffer();
+        let genemap = super::GeneMap::new_from_buffer(buffer.as_bytes()).unwrap();
+        let library = build_invalid_library();
+        assert!(!genemap.validate_library(&library));
+    }
+
+    #[test]
+    fn test_from_file() {
+        let filepath = "example/g2s.txt";
+        let genemap = super::GeneMap::new(filepath).unwrap();
+        assert_eq!(genemap.get(b"lib.0").unwrap(), &b"gene.0"[..]);
+        assert_eq!(genemap.get(b"lib.99").unwrap(), &b"gene.9"[..]);
+    }
+
 }
